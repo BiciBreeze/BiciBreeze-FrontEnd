594b5c24f18f81a686d71d131596deb7b07cb434
Mathifa <u20221a955@upc.edu.pe>
2024-09-25 03:47:22 -0500

    feat:Update UserProfile page up to merge

--- src/context/Registration/components/header-registration.component.vue
+++ src/context/Registration/components/header-registration.component.vue
@@ -40,7 +40,7 @@
             <transition enter-active-class="transition ease-out duration-100" enter-from-class="transform opacity-0 scale-95" enter-to-class="transform opacity-100 scale-100" leave-active-class="transition ease-in duration-75" leave-from-class="transform opacity-100 scale-100" leave-to-class="transform opacity-0 scale-95">
               <MenuItems class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
                 <MenuItem v-slot="{ active }">
-                  <a href="#" :class="[active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700']">Your Profile</a>
+                  <router-link :to="'/user-profile'" :class="[active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700']"> Profile </router-link>
                 </MenuItem>
                 <MenuItem v-slot="{ active }">
                   <a href="#" :class="[active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700']">Settings</a>
@@ -72,7 +72,6 @@ const navigation = [
   { name: 'Home', href: '/home', current: true },
   { name: 'Bookings', href: '/bookings', current: false },
   { name: 'Subscription', href: '/subscription', current: false },
-
 ];
 </script>
 
